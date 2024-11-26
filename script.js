999c083322f2c89048fef6dc4cd0ecc6be0f3c1e
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-11-26 12:09:05 +0000

    Add functionality to add books

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ function Book(name, author, numberPages) {
 
 const openButton = document.querySelector("#open-modal");
 const closeButton = document.querySelector("#close-button");
+const form = document.querySelector("form");
+const modal = document.querySelector("dialog");
 
 openButton.addEventListener("click", () => modal.showModal());
 closeButton.addEventListener("click", () => modal.close());
+form.addEventListener("submit", () => {
+    const name = document.querySelector("#name").value;
+    const author = document.querySelector("#author").value;
+    const numberPages = document.querySelector("#pages").value;
+    let book = new Book(name, author, numberPages);
+    books.push(book);
+    form.reset();
+    modal.close();
+});
