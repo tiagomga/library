36e3b996607917f376b5b3fa21444d7e659bde63
Tiago Antão <109687852+tiagomga@users.noreply.github.com>
2024-11-26 13:08:57 +0000

    Add functionality to insert books

--- script.js
+++ script.js
@@ -7,6 +7,62 @@ function Book(name, author, numberPages) {
     this.read = false;
 }
 
+function createCardItem(name, numberPages, author, read) {
+    const card = document.createElement("div");
+    const cardContent = document.createElement("div");
+    const cardButtons = document.createElement("div");
+
+    card.classList.add("card");
+    cardContent.classList.add("card-content");
+    cardButtons.classList.add("card-buttons");
+
+    card.appendChild(cardContent);
+    card.appendChild(cardButtons);
+
+    const bookName = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookAuthor = document.createElement("p");
+    const readStatus = document.createElement("p");
+
+    bookName.textContent = name;
+    bookPages.textContent = numberPages + " pages";
+    bookAuthor.textContent = author;
+    readStatus.textContent = read ? "Read" : "Not read" ;
+
+    bookName.classList.add("book-name");
+    bookPages.classList.add("book-pages");
+    bookAuthor.classList.add("book-author");
+    readStatus.classList.add("read-status");
+
+    cardContent.appendChild(bookName);
+    cardContent.appendChild(bookPages);
+    cardContent.appendChild(bookAuthor);
+    cardContent.appendChild(readStatus);
+
+    const markAsReadButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    markAsReadButton.classList.add("mark-read");
+    deleteButton.classList.add("delete");
+
+    markAsReadButton.textContent = read ? "Mark as unread" : "Mark as read";
+    deleteButton.textContent = "Remove";
+
+    cardButtons.appendChild(markAsReadButton);
+    cardButtons.appendChild(deleteButton);
+
+    return card;
+}
+
+function updateBookCards() {
+    const booksDiv = document.querySelector(".books");
+    booksDiv.replaceChildren();
+    for (let book of books) {
+        const bookCard = createCardItem(book.name, book.numberPages, book.author, book.read);
+        booksDiv.appendChild(bookCard);
+    }
+}
+
 const openButton = document.querySelector("#open-modal");
 const closeButton = document.querySelector("#close-button");
 const form = document.querySelector("form");
@@ -20,6 +76,7 @@ form.addEventListener("submit", () => {
     const numberPages = document.querySelector("#pages").value;
     let book = new Book(name, author, numberPages);
     books.push(book);
+    updateBookCards();
     form.reset();
     modal.close();
 });
